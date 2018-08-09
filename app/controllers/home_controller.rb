a2f30e7dbf3af1fe2911756f1ed695bac0b39b62
pashko37 <evobbs5@gmail.com>
2018-08-09 22:58:51 +0300

    Add page contacts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   protect_from_forgery with: :exception
 
-      def index
-          render "home/index"
-      end
+  def index
+    render 'home/index'
+  end
 end
