066721716c90961422d1766a191230529134b5e8
pashko37 <evobbs5@gmail.com>
2018-08-14 00:55:05 +0300

    Change design

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "My", password: "secret", except: [:index, :show]
+
 
   def index
     @articles = Article.all
